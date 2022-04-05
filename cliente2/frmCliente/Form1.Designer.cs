f342039a40776d934eb33fec923f69b957e18204
gustavo-fernando-tabora18 <98550315+gustavo-fernando-tabora18@users.noreply.github.com>
2022-04-05 16:12:34 -0600

    Se agrego proyecto

---
+++ cliente2/frmCliente/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace cliente2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.btnDatosUsuario = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(147, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mensaje cliente:";
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(297, 187);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(75, 23);
+            this.btnEnviar.TabIndex = 1;
+            this.btnEnviar.Text = "Enviar datos";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // btnDatosUsuario
+            // 
+            this.btnDatosUsuario.Location = new System.Drawing.Point(257, 87);
+            this.btnDatosUsuario.Name = "btnDatosUsuario";
+            this.btnDatosUsuario.Size = new System.Drawing.Size(172, 20);
+            this.btnDatosUsuario.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(583, 316);
+            this.Controls.Add(this.btnDatosUsuario);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox btnDatosUsuario;
+    }
+}
+
---
+++ cliente2/frmCliente/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace cliente2
+{
+    
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+
+            Socket listen = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint connect = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 6400);
+            listen.Connect(connect);
+            byte[] enviar_Info = new byte[100];
+            string dato;
+            dato = btnDatosUsuario.Text;
+            enviar_Info = Encoding.Default.GetBytes(dato);
+            listen.Send(enviar_Info);
+        }
+    }
+}
